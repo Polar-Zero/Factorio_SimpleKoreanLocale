eb8cd4fe8fd4377c1cc025e70925432f57345c67
PolarZero <trinity5000@naver.com>
2024-11-02 10:04:35 +0900

    0.2.1

--- data-updates.lua
+++ data-updates.lua
@@ -1,23 +1,14 @@
 ---   PLEASE CONTACT ME BEFORE COPYING ANY PART OF MY MODS. SEE LICENSE
 ---   - PolarZero
 
-function setI18n(p,o,l,a,r) -- Version 2.0
-	if data.raw[o] then
-		if data.raw[o][l] then
-			data.raw[o][l].localised_name={a..'-name.'..r..'-'..l}
-			if p then
-				data.raw[o][l].localised_description={a..'-description.'..r..'-'..l}
-			end
-		end
-	end
-end
-function setRecipe(p,o,l,a)
-	setI18n(p,'item',l,o,a); -- true, item,  automation-science-pack,item,  baketorio -> item-name.baketorio-automation-science-pack
-	setI18n(p,'recipe',l,o,a);
-end
-function setI18nR(p,o,l,a)
-	setI18n(p,o,l,o,a);
-end
+---   Code Version 2.1
+function setI18n(p,o,l,a,r) if data.raw[o] then if data.raw[o][l] then data.raw[o][l].localised_name={a..'-name.'..r..'-'..l}
+if p then data.raw[o][l].localised_description={a..'-description.'..r..'-'..l} end end end end
+function setRecipe(p,o,l,a) setI18n(p,'item',l,o,a) setI18n(p,'recipe',l,o,a) end
+function setI18nR(p,o,l,a) setI18n(p,o,l,o,a); end
+function enforceI18n(p,z,o) if data.raw[p] then if data.raw[p][z] then data.raw[p][z].localised_name={p..'-name.'..z}
+if o then data.raw[p][z].localised_description={p..'-description.'..z} end end end end
+
 if mods['wret-beacon-rebalance-mod'] then
 	setRecipe(true,'entity','beacon','wr');
 	setI18n(true,'beacon','beacon','entity','wr');
@@ -52,4 +43,49 @@ if mods['RibbonMaze'] or mods['RibbonMaze20'] then
 	setRecipe(true,'item','landfill','ribbonmaze');
 	setRecipe(true,'item','electric-mining-drill','ribbonmaze');
 	setI18nR(true,'tile','water-green','ribbonmaze');
+end
+if mods['ev-refining'] then
+	enforceI18n('item','crusher1');
+	enforceI18n('recipe','crusher1');
+	enforceI18n('assembling-machine','crusher1');
+	enforceI18n('item','find-sand');
+	enforceI18n('item','gravel');
+	enforceI18n('item','iron-dust');
+	enforceI18n('item','iron-clump');
+	enforceI18n('item','iron-chunk');
+	enforceI18n('item','copper-dust');
+	enforceI18n('item','copper-clump');
+	enforceI18n('item','copper-chunk');
+	enforceI18n('recipe','steel-dust-smelting');
+	enforceI18n('recipe','sand-to-brick');
+	enforceI18n('recipe','sand-landfill');
+	enforceI18n('recipe','gravel-landfill');
+	enforceI18n('recipe','gravel-to-brick');
+	enforceI18n('recipe','gravel-to-sand');
+	enforceI18n('recipe','concrete-finishing');
+	enforceI18n('recipe','stone-to-gravel');
+	enforceI18n('recipe','coal-dust');
+	enforceI18n('recipe','coal-clump');
+	enforceI18n('recipe','coal-chunk');
+	enforceI18n('recipe','advanced-coal-crushing');
+	enforceI18n('recipe','advanced-coal-enriching');
+	enforceI18n('recipe','advanced-steel-processing');
+	enforceI18n('recipe','coal-dust-enrichment');
+	enforceI18n('recipe','coal-clump-enrichment');
+	enforceI18n('recipe','coal-chunk-enrichment');
+	enforceI18n('recipe','coal-coke-enrichment');
+	enforceI18n('recipe','iron-ore-purifying');
+	enforceI18n('recipe','iron-ore-enriching');
+	enforceI18n('recipe','iron-ore-crushing');
+	enforceI18n('recipe','iron-ore-alternative-smelting');
+	enforceI18n('recipe','iron-chunk-processing');
+	enforceI18n('recipe','iron-clump-processing');
+	enforceI18n('recipe','iron-dust-smelting');
+	enforceI18n('recipe','copper-ore-purifying');
+	enforceI18n('recipe','copper-ore-enriching');
+	enforceI18n('recipe','copper-ore-crushing');
+	enforceI18n('recipe','copper-ore-alternative-smelting');
+	enforceI18n('recipe','copper-chunk-processing');
+	enforceI18n('recipe','copper-clump-processing');
+	enforceI18n('recipe','copper-dust-smelting');
 end
