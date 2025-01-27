b041b70feaa815b87e057737db9f340fa9997666
PolarZero <trinity5000@naver.com>
2025-01-27 18:45:09 +0900

    0.7.5

--- updates/LunarLandings.lua
+++ updates/LunarLandings.lua
@@ -5,3 +5,4 @@
 require("SKL");
 setI18nR(true,'technology','rocket-silo','lunarland');
 setI18nR(true,'technology','space-science-pack','lunarland');
+setI18nR(true,'tool','space-science-pack','lunarland');
