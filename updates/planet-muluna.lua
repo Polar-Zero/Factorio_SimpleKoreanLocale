2b8a6fea5fa14790cf10bcec07b495b7050542f5
PolarZero <trinity5000@naver.com>
2025-09-24 13:55:36 +0900

    0.13.2

--- updates/planet-muluna.lua
+++ updates/planet-muluna.lua
@@ -2,3 +2,5 @@ require("SKL");
 setI18nR(true,'tips-and-tricks-item','space-science','muluna');
 setI18nR(true,'technology','automation-3','muluna');
 setI18nR(true,'technology','advanced-asteroid-processing','muluna');
+setI18nR(true,'technology','advanced-asteroid-productivity','muluna');
+setI18nR(true,'technology','advanced-asteroid-reprocessing','muluna');
